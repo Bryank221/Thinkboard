320682fc35609853babf03dde992b4404134dd9e
BLUBTOP\bryan <bryank221@gmail.com>
2025-12-10 22:02:52 +0800

    initial commit

---
+++ backend/src/models/Note.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from "mongoose";
+
+// 1. create schema
+// 2. model based of that schema
+
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Note = mongoose.model("Note", noteSchema)
+
+export default Note;
